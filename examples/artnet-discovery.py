e786746df952592d25868eb584faaf5fe4ac711e
Fredrik Lindberg <fli@shapeshifter.se>
2013-07-27 18:56:50 +0200

    Add usage example programs
    artnet-discovery.py
     Lists all discovered artnet nodes
    
    artnet-setdmx.py
     Sends a custom DMX512 frame
    
    Signed-off-by: Fredrik Lindberg <fli@shapeshifter.se>

---
+++ examples/artnet-discovery.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# Copyright (C) 2013 Fredrik Lindberg <fli@shapeshifter.se>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+
+from artnet import ArtnetController
+from select import select
+from optparse import OptionParser
+from time import time
+
+parser = OptionParser(usage="%prog [options]")
+parser.add_option("-t", dest="timeout", default=3,
+                  help="Discovery timeout")
+(opts, args) = parser.parse_args()
+
+ac = ArtnetController("pyartnet-discover")
+ac.discover()
+
+start = time()
+while (time() - start) < int(opts.timeout):
+	readable, writeable, exception = select([ac], [], [], 1)
+	if len(readable) > 0:
+		ac.run()
+
+for node in ac.nodes():
+	print "IP: " + node.ip + " (" + node.mac + ")"
+	print "Name: " + node.name,
+	print ", version: " + str(node.version)
+	print "Subnet: " + str(node.subnet)
+	print "Ports: " + str(node.ports)
---
+++ examples/artnet-setdmx.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# Copyright (C) 2013 Fredrik Lindberg <fli@shapeshifter.se>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+
+from artnet import ArtnetController, DmxPort
+from select import select
+from optparse import OptionParser
+from itertools import izip
+
+def pairwise(iterable):
+    a = iter(iterable)
+    return izip(a, a)
+
+parser = OptionParser(usage="%prog [options] channel value [channel value [...]]")
+parser.add_option("-p", "--port", dest="port", default=0,
+                  help="Set port address")
+(opts, args) = parser.parse_args()
+
+if len(args) % 2:
+    parser.error("Channel and values should be specified in pairs")
+
+ac = ArtnetController("pyartnet-setdmx")
+dp = DmxPort(opts.port, DmxPort.INPUT)
+ac.add_port(dp)
+
+print "Using port", dp
+
+for channel, value in pairwise(args):
+    dp.set(int(channel), int(value))
+    print "Channel", channel, "set to", value
+dp.send()
