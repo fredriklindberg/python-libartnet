872e8b96f19424d08b6f33548f2e4be98c251e6d
Fredrik Lindberg <fli@shapeshifter.se>
2015-03-15 14:52:05 +0100

    Add py-versioneer for automatic version numbering
    Signed-off-by: Fredrik Lindberg <fli@shapeshifter.se>

--- artnet/__init__.py
+++ artnet/__init__.py
@@ -18,3 +18,7 @@
 
 from . import port
 from .artnet import Controller
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
--- setup.py
+++ setup.py
@@ -5,8 +5,16 @@ try:
 except ImportError:
     from distutils.core import setup
 
+import versioneer
+versioneer.VCS = 'git'
+versioneer.versionfile_source = 'artnet/_version.py'
+versioneer.versionfile_build = None
+versioneer.tag_prefix = 'v'
+versioneer.parentdir_prefix = 'py-artnet-'
+
 setup(name="py-artnet",
-      version=0,
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
       description='Python bindings for libartnet',
       author='Fredrik Lindberg',
       author_email='fli@shapeshifter.se',
